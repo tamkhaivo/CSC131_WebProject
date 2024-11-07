b783d0f91acf12afdbbc77d7af5e7caa1bdb9233
i-got-to-eat <khaitamvo@gmail.com>
2024-11-07 13:10:37 -0800

    t3-stack initalize DATABASE and AUTH

--- pass-the-test.com/src/server/auth/config.ts
+++ pass-the-test.com/src/server/auth/config.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { db } from "~/server/db";
 
 /**
@@ -32,16 +32,14 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+        GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
   ],
   adapter: PrismaAdapter(db),
   callbacks: {
