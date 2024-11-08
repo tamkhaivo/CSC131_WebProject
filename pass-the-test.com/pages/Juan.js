55c4faba093ae040ef833a87256598edd16331b3
i-got-to-eat <khaitamvo@gmail.com>
2024-11-08 10:23:33 -0800

    TypeScript - Requirement

--- pass-the-test.com/pages/Juan.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (<main>
-        <p>This is a test website</p>
-        <h1>Trying to remember all the HTML stuff</h1>
-        <h2>This will take me time to remember</h2>
-    </main>
-    );
-}  
--- pass-the-test.com/pages/aaron.js
+++
@@ -1,9 +0,0 @@
-export default function Home(){
-  return (
-      <main>
-      <h1>
-        This is a test.
-      </h1>
-        </main>
-      );
-}
