73e041974c7b8cf8279bf3eac4affc9da733ea46
i-got-to-eat <khaitamvo@gmail.com>
2024-11-24 15:54:51 -0800

    Updated Home Page

--- pass-the-test.com/src/app/page.tsx
+++ pass-the-test.com/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
-import SearchBar  from "~/app/_components/search";
+import SearchBar from "~/app/_components/search";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
@@ -20,53 +20,42 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <SearchBar/>
+          <SearchBar />
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-              <span className="text-[hsl(280,100%,70%)]">Studying made easy</span> so you can focus on the things that matter!
+            <span className="text-[hsl(280,100%,70%)]">Studying made easy</span>{" "}
+            so you can focus on the things that matter!
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
+              href="#_testimonials"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">Testimonials →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Look what other people had to say about our service!
               </div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
+              href="/contact"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-bold">Contact Us →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Got an idea? Let us know how we can improve!
               </div>
             </Link>
           </div>
+
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
           </div>
 
           {session?.user && <LatestPost />}
+
+
+          
         </div>
       </main>
     </HydrateClient>
