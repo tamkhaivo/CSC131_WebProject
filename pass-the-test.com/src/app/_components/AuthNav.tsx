d74c6f1d6af728a01c9cdcae0267b3bd68deb57d
i-got-to-eat <khaitamvo@gmail.com>
2024-11-25 14:20:39 -0800

    Added Responsive Navigation

--- pass-the-test.com/src/app/_components/AuthNav.tsx
+++ pass-the-test.com/src/app/_components/AuthNav.tsx
@@ -9,11 +9,11 @@ interface AuthNavProps {
 
 export default function AuthNav({ session }: AuthNavProps) {
   return session ? (
-    <Link href="/api/auth/signout" className="p-2 text-xl hover:underline">
+    <Link href="/api/auth/signout" className="p-2 text-xl hover:underline flex items-center gap-2 block text-white hover:bg-gray-600 hover:underline">
       Logout {session.user?.name}
     </Link>
   ) : (
-    <Link href="/api/auth/signin" className="p-2 text-xl hover:underline">
+    <Link href="/api/auth/signin" className="p-2 text-xl hover:underline flex items-center gap-2 block text-white hover:bg-gray-600 hover:underline">
       Login
     </Link>
   );
