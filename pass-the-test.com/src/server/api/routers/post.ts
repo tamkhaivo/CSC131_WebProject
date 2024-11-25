6650e9b26c78e895bf6cfae38bafa586c2b49b63
i-got-to-eat <khaitamvo@gmail.com>
2024-11-24 19:12:22 -0800

    updated API to get first 10

--- pass-the-test.com/src/server/api/routers/post.ts
+++ pass-the-test.com/src/server/api/routers/post.ts
@@ -7,6 +7,38 @@ import {
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
+  getAllProducts: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const products = await ctx.db.product.findMany({
+        take: 10,
+      });
+      return products;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw new Error("Failed to fetch products");
+    }
+  }),
+  insertData: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        desc: z.string(),
+        price: z.number(),
+        sale: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.product.create({
+        data: {
+          title: input.title,
+          desc: input.desc,
+          price: input.price,
+          sale: input.sale,
+        },
+      });
+    }),
+  /*
+  TO DO: INTEGRATE TRCP ROUTER DB CALLS
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -14,8 +46,6 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  /*
-  TO DO: INTEGRATE TRCP ROUTER DB CALLS
   create: protectedProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -35,25 +65,9 @@ export const postRouter = createTRPCRouter({
 
     return post ?? null;
   }),
-
-  */
-  getAllProducts: publicProcedure.query(async ({ ctx }) => {
-    const products = await ctx.db.product.findMany();
-    return products;
-  }),
-
-  insertData: publicProcedure
-    .input(z.object({ title: z.string(), desc: z.string(), price: z.number() }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.product.create({
-        data: {
-          title: input.title,
-          desc: input.desc,
-          price: input.price,
-        },
-      });
-    }),
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+  */
 });
