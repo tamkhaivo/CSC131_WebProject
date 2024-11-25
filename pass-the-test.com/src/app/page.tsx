2029de75dc7acb2f8cdeab8e1401e3a8e60f27b1
i-got-to-eat <khaitamvo@gmail.com>
2024-11-24 17:01:24 -0800

    Removed Duplicate Payment

--- pass-the-test.com/src/app/page.tsx
+++ pass-the-test.com/src/app/page.tsx
@@ -54,10 +54,7 @@ export default async function Home() {
             </p>
           </div>
 
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-3xl font-bold">Test Payment</h2>
-            <PaymentForm />
-          </div>
+ 
 
           {session?.user && <LatestPost />}
 
